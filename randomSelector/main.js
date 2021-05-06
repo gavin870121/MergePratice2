8415b2827d6348ae113f647a2f0ceedfa7319a3b
gavin870121 <gavin870121@gmail.com>
2021-05-06 16:09:58 +0800

    1609

--- randomSelector/main.js
+++ randomSelector/main.js
@@ -3,11 +3,11 @@ $(function(){
     $("input").on("click",function(){
         //alert("Hi");
         //$("h1").text("Hello");
-        var numberOfListItem = $("li").length;
+        var numberOfListItem = $("li#food").length;
         var randomChildNumber = Math.floor(Math.random()*numberOfListItem);
-        $("h1").text($("li").eq(randomChildNumber).text());
+        $("h1").text($("li#food").eq(randomChildNumber).text());
         //找到img元件 V
         //設定img元件的src屬性 -> 對應元件的title值
-        $("Img").attr("src",$("li").eq(randomChildNumber).attr("title"));
+        $("Img").attr("src",$("li#food").eq(randomChildNumber).attr("title"));
     });
 });
